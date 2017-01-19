6965fb3ef37f0240f44beaa3d8b6ca9c21cecbd2
paweljurczynski <pawel.jurczynski@live.com>
2017-01-19 22:11:35 +0100

    layout

--- client/main.js
+++ client/main.js
@@ -7,5 +7,6 @@ import '../imports/styles/ng-table.min.css';
 import '../imports/styles/fonts.googleapis.css';
 import '../imports/styles/w3.css';
 import '../imports/styles/main.css';
+import '../imports/styles/style.scss';
 
 import { name as RespondentBroker } from '../imports/ui/components/respondentBroker/respondentBroker';
