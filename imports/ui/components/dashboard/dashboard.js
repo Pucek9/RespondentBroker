0189b2d50523548b2b3cde2b19f8d244e44de5b8
Michał Gruca <pucek9@gmail.com>
2017-01-20 06:40:45 +0100

    removed idea files

--- imports/ui/components/dashboard/dashboard.js
+++ imports/ui/components/dashboard/dashboard.js
@@ -3,6 +3,9 @@ import angularMeteor from 'angular-meteor';
 import uiRouter from 'angular-ui-router';
 import {DASHBOARD as PAGE} from '../../../helpers/constants';
 import template from './dashboard.html';
+import {Meteor} from 'meteor/meteor';
+import {Projects} from '../../../api/projects';
+import {Responses} from '../../../api/responses';
 
 class Controller {
 
@@ -16,20 +19,27 @@ class Controller {
 			users() {
 				return Meteor.users.find({});
 			},
-			projects() {
+			myProjects() {
 				return Projects.find({owner: this.userId});
 			},
-			responses() {
-				return Responses.find({});
-			},
-			points() {
-				let users = Meteor.users.find({});
-				var sum=0;
-				users.forEach(function(user){
-					sum = sum + user.profile.points;
+			myProjectsResponses() {
+				let sum=0;
+				let projects = Projects.find({owner: this.userId});
+				projects.forEach(function(project){
+					sum = sum + project.responses.length;
 				});
 				return sum;
 			},
+			responsesWithoutPay() {
+				let sum=0;
+				let projects = Projects.find({owner: this.userId});
+				projects.forEach(function(project){
+					let responses = Responses.find({project: project._id, isPaid: false}).fetch();
+					console.log(responses)
+					sum = sum + responses.length
+				});
+				return sum;
+			}
 		});
 	}
 }
